1adb98da7c17ff45efe9c701f711a2e9bc089024
keshavsoft <nknnkumar@yahoo.com>
2026-05-28 18:16:31 +0530

    valid

--- bin/v2/AppJs/UpdateAppJs/index.js
+++ bin/v2/AppJs/UpdateAppJs/index.js
@@ -1,17 +1,30 @@
+// v2/AppJs/index.js
+
 import fs from "fs";
 
 import updateImports from "./updateImports/index.js";
 import updateAppUse from "./updateUse/index.js";
 
+import validateEndpoint from "./validations/validateEndpoint.js";
+import validateAppJsPath from "./validations/validateAppJsPath.js";
+
 const updateAppJs = ({ appJsPath, endpoint, showLog = false }) => {
-    if (!fs.existsSync(appJsPath)) {
-        if (showLog) console.log("app.js file not found ?");
+    validateEndpoint({ endpoint });
 
-        return false;
-    };
+    validateAppJsPath({
+        appJsPath,
+        fs
+    });
 
-    updateImports({ appJsPath, endpoint });
-    updateAppUse({ appJsPath, endpoint });
+    updateImports({
+        appJsPath,
+        endpoint
+    });
+
+    updateAppUse({
+        appJsPath,
+        endpoint
+    });
 
     return false;
 };
