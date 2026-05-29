bfef2e11f7ebfb157d51c179bc22720facc8e70a
keshavsoft <nknnkumar@yahoo.com>
2026-05-29 10:07:26 +0530

    use test

--- bin/v5/AppJs/UpdateAppJs/index.js
+++ bin/v5/AppJs/UpdateAppJs/index.js
@@ -5,11 +5,16 @@ import fs from "fs";
 import updateAppUse from "./updateUse/index.js";
 
 import forImport from "./forImport.js";
+import forUse from "./forUse.js";
 
 const updateAppJs = ({ appJsPath, endpoint, showLog = false }) => {
     const fromImports = forImport({ appJsPath, endpoint, showLog });
 
     if (showLog) console.log("fromImports : ", fromImports);
+
+    const fromUse = forUse({ appJsPath, endpoint, showLog });
+
+    if (showLog) console.log("fromUse : ", fromUse);
 
     return false;
 };
