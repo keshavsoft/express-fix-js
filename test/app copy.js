e2b3b851139ff2d025f0bec0ca7a4d287f9748f6
keshavsoft <nknnkumar@yahoo.com>
2026-05-29 10:08:18 +0530

    test

--- test/app copy.js
+++ test/app copy.js
@@ -4,14 +4,28 @@ import http from 'http';
 
 import { StartFunc as StartFuncFromWebSocketServer } from "./Projects/WebSocketServer/V2/entryFile.js";
 
+import { router as routerFromMetaData } from "./MetaData/routes.js";
+
+import { router as routerFromApi } from "./Api/routes.js";
+import { router as routerFromV4 } from "./V4/routes.js";
+import { router as WhatsApp } from "./Projects/WhatsApp/routes.js";
+import { router as routerFromSecured } from "./Secured/routes.js";
+
 const app = express()
 
 const server = http.createServer(app);
 
 var port = normalizePort(process.env.PORT || 3000);
-
 app.use(express.static('Public'));
 app.use(cookieParser());
+
+app.use("/MetaData", routerFromMetaData);
+
+app.use("/Api", routerFromApi);
+app.use("/V4", routerFromV4);
+app.use("/WhatsApp", WhatsApp);
+
+app.use("/Secured", routerFromSecured);
 
 StartFuncFromWebSocketServer(server);
 
